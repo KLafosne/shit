d9d13bc219c72ceb2c44ee41d6f5edcd220d36c3
KLafosne <109802795+KLafosne@users.noreply.github.com>
2024-01-17 17:30:06 +0800

    Update shit.py

--- shit.py
+++ shit.py
@@ -265,9 +265,24 @@ def update_warp_proxy_groups(config_warp_data, merged_proxies):
 merged_proxies = []
 
 # 处理 clash URLs
-process_urls('./urls/clash_shit_urls.txt', process_clash)
+process_urls('./urls/clash_shit_urls.txt', process_clash_shit)
 
-process_urls('./urls/proxy', process_clash)
+process_urls('./urls/proxy.txt', process_proxy)
+
+process_urls('./urls/clash_new_urls.txt', process_clash)
+
+# 处理 shadowtls URLs
+process_urls('./urls/sb_urls.txt', process_sb)
+
+# 处理 hysteria URLs
+process_urls('./urls/hysteria_urls.txt', process_hysteria)
+
+# 处理 hysteria2 URLs
+process_urls('./urls/hysteria2_urls.txt', process_hysteria2)
+
+# 处理 xray URLs
+process_urls('./urls/xray_urls.txt', process_xray)
+
 # 读取普通的配置文件内容
 with open('./templates/clash_template.yaml', 'r', encoding='utf-8') as file:
     config_data = yaml.safe_load(file)
@@ -282,7 +297,7 @@ else:
 update_proxy_groups(config_data, merged_proxies)
 
 # 将更新后的数据写入到一个YAML文件中，并指定编码格式为UTF-8
-with open('./sub/shit.yaml', 'w', encoding='utf-8') as file:
+with open('./sub/clash.yaml', 'w', encoding='utf-8') as file:
     yaml.dump(config_data, file, sort_keys=False, allow_unicode=True)
 
 print("聚合完成")
